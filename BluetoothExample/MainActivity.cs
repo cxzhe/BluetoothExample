2703c0a7912ec22ab6d0addac6116588d5eed8d3
xianzhe.chen <xianzhe.chen@pasasoft.com>
2019-05-31 18:18:11 +0800

    new

--- BluetoothExample/MainActivity.cs
+++ BluetoothExample/MainActivity.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using Android;
 using Android.App;
+using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
 using Android.Support.V7.App;
 using Android.Views;
-using Android.Widget;
 
 namespace BluetoothExample
 {
@@ -19,16 +23,31 @@ namespace BluetoothExample
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            //Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+
+
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
         }
 
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+        const string _pkgName = "com.companyname.bluetoothexample";
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+        }
+
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+
             return true;
         }
 
@@ -46,15 +65,51 @@ namespace BluetoothExample
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
             View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            Test();
+            //Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                //.SetAction("Action", OnClick).Show();
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        public void OnClick(View v)
+        {
+            Console.WriteLine("OnClick");
+        }
+
+        const int _ccode = 777;
+
+        public void Test()
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                var ss = ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.AccessFineLocation);
+                var per = ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation);
+                var permission = PackageManager.CheckPermission(Manifest.Permission.AccessFineLocation, _pkgName);
+                if (ss)
+                {
+                    var view = this.FindViewById(Resource.Id.main_view);
+                    Snackbar.Make(view, "Need permission", Snackbar.LengthLong).Show();
+                    //.SetAction("Action", OnClick).Show();
+                    //DialogFragment dialogFragment = new Android.App.DialogFragment();
+                    Android.App.FragmentTransaction transcation = FragmentManager.BeginTransaction();
+                }
+
+                    ActivityCompat.RequestPermissions(this, new[] { Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessCoarseLocation }, _ccode);
+
+            }
+            else
+            {
+
+            }
+        }
+
+
     }
 }
 
